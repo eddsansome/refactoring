4f0b9e74546d594451baed60c18a9d9821517d07
eddsansome <edward.sansome@smartpension.co.uk>
2022-06-08 17:13:14 +0100

    oh dear

--- lib/refactoring/chapters/chapter_1/generate_statement.rb
+++ lib/refactoring/chapters/chapter_1/generate_statement.rb
@@ -1,12 +1,101 @@
+# frozen_string_literal: true
+
 module Refactoring
   module Chapters
     module Chapter1
-      class GenerateStatement
 
+      class OutputStatement
+        def initialize(statement)
+          @statement = statement
+        end
+
+        def std_out
+          result = "Statement for #{statement.customer}\n"
+          statement.line_items.each do |line|
+            result << "    #{line.play_name}: #{format_amount(line.total_amount / 100)} (#{line.audience} seats)\n"
+          end
+          result << "Amount owed is #{format_amount(statement.invoice_total / 100)}\n"
+          result << "You earned #{statement.volume_credits} credits\n"
+        end
+        private 
+
+        attr_reader :statement
+
+        def format_amount(num)
+          format("$%.2f", num)
+        end
+
+
+
+      end
+
+      class StatementLine
+        def initialize(play:, audience:)
+          @play = play
+          @audience = audience
+          @total_amount = 0
+          @volume_credits = 0
+        end
+
+        def process
+          add_credit
+          add_charge
+          self
+        end
+
+        def play_name
+          @play['name']
+        end
+
+        attr_reader :total_amount, :volume_credits, :audience
+
+        private
+
+        def add_credit
+          @volume_credits += [@audience - 30, 0].max
+          @volume_credits += (@audience / 5).floor if @play == "comedy"
+        end
+        def add_charge
+          case @play['type']
+          when "tragedy"
+            @total_amount = 40_000
+            @total_amount += 1000 * (@audience - 30) if @audience > 30
+          when "comedy"
+            @total_amount = 30_000
+            @total_amount += 10_000 + 500 * (@audience - 20) if @audience > 20
+            @total_amount += 300 * @audience
+          else
+            raise "unknown type: #{@play}"
+          end
+        end
+      end
+
+      class Statement
+        attr_reader :customer, :line_items
+
+        def initialize(customer:)
+          @customer = customer
+          @line_items = []
+        end
+
+        def add_line_item(play:,audience:)
+          @line_items <<  StatementLine.new(play: play, audience: audience).process
+        end
+
+        def invoice_total
+          @line_items.sum {|line| line.total_amount }
+        end
+
+        def volume_credits
+          @line_items.sum {|line| line.volume_credits }
+        end
+
+      end
+
+      class GenerateStatement
         def self.call(...)
           new(...).call
         end
-
 
         def initialize(invoice:, plays:)
           @invoice = invoice
@@ -14,59 +103,21 @@ module Refactoring
         end
 
         def call
-          total_amount = 0
-          volume_credits = 0
-          result = "Statement for #{invoice["customer"]}\n"
-
+          statement = Statement.new(customer: invoice['customer'])
           invoice["performances"].each do |perf|
-            play = plays[perf["playID"]]
-
-            this_amount = amount_for(perf, play)
-
-            # add volume credits
-            volume_credits += [perf["audience"] - 30, 0].max
-            # add extra credit for every ten comedy attendees
-            volume_credits += (perf["audience"] / 5).floor if play["type"] == "comedy"
-
-            # print line for this order
-            result << "    #{play["name"]}: #{format_amount(this_amount / 100)} (#{perf["audience"]} seats)\n"
-            total_amount += this_amount
+            statement.add_line_item(play: play(perf["playID"]), audience: perf["audience"])
           end
-
-          result << "Amount owed is #{format_amount(total_amount / 100)}\n"
-          result << "You earned #{volume_credits} credits\n"
-          result
+          OutputStatement.new(statement).std_out
         end
 
 
         private
 
+        def play(play_id)
+          plays[play_id]
+        end
+
         attr_reader :invoice, :plays
-
-        def format_amount(num)
-          format("$%.2f", num)
-        end
-
-
-        def amount_for(a_performance, play)
-          result = 0
-
-          case play["type"]
-          when "tragedy"
-            result = 40_000
-            result += 1000 * (a_performance["audience"] - 30) if a_performance["audience"] > 30
-          when "comedy"
-            result = 30_000
-            result += 10_000 + 500 * (a_performance["audience"] - 20) if a_performance["audience"] > 20
-            result += 300 * a_performance["audience"]
-          else
-            raise "unknown type: #{play["type"]}"
-          end
-
-          result
-        end
-
-
 
       end
     end
